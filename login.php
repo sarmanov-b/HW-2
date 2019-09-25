4e476a0408ddeb879f4ee88aea5c556d8241dc71
sarmanov-b <55474703+sarmanov-b@users.noreply.github.com>
2019-09-25 17:57:08 +0600

    login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<div style="margin-left: 50%">
+		<form action="process.php" method="POST">
+			<p>
+				<label>Login</label>
+				<input type="text" name="username">
+			</p>
+			<p>
+				<label>Password</label>
+				<input type="password" name="password">
+			</p>
+			<p>
+				<input type="submit" value="Login">
+			</p>
+		</form>
+	</div>
+</body>
+</html>
